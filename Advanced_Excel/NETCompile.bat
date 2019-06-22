9997746383f1fb2db83c80186a14c311d09d44d7
Intimidator <hanno@Hanno.co.za>
2019-06-23 00:02:07 +0200

    Renamed Extension to remove version number

--- Advanced_Excel/NETCompile.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" "C:\Source\Repos\outsystems_advancedexcel\Advanced_Excel\Source\NET\Advanced_Excel.sln" /nologo /verbosity:minimal /target:Rebuild /property:Configuration=Release
